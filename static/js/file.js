33713fe0d82e8478f882710310d01e79e53a0366
Juan Balbuena <ianivigo2@gmail.com>
2016-04-08 08:51:22 +0200

    commit inicial

--- static/js/file.js
+++ static/js/file.js
@@ -1,3 +1,19 @@
 /**
  * Created by Juan on 02/03/2016.
  */
+
+
+var avatarContainer = document.getElementById("avatar");
+
+var avatar1 = "./static/img/juan-a.jpg";
+var avatar2 = "./static/img/juan-b.jpg";
+
+avatarContainer.addEventListener("mouseover", function(){
+        avatarContainer.src = avatar2;
+    });
+
+avatarContainer.addEventListener("mouseout", function(){
+        avatarContainer.src =  avatar1;
+    });
+
+
