b16ed34e92dd4374d7cebc92004494e4ae97626a
Juan Balbuena <ianivigo2@gmail.com>
2016-04-08 14:04:36 +0200

    Creada uan mini funcion en js para cambiar de imagen

--- static/js/file.js
+++ static/js/file.js
@@ -2,18 +2,24 @@
  * Created by Juan on 02/03/2016.
  */
 
-
-var avatarContainer = document.getElementById("avatar");
-
-var avatar1 = "./static/img/juan-a.jpg";
-var avatar2 = "./static/img/juan-b.jpg";
-
-avatarContainer.addEventListener("mouseover", function(){
+(function () {
+    // GUARDAR ETIQUETA IMG PARA CAMBIAR SRC
+    var avatarContainer = document.getElementById("avatar");
+    // GUARDAR URL ABSOLUTAS
+    var avatar1 = "./static/img/juan-a.jpg";
+    var avatar2 = "./static/img/juan-b.jpg";
+    // AÑADIR EVENTO DE ENTRADA A LA ETIQUETA IMG
+    avatarContainer.addEventListener("mouseover", entraMouse);
+    // ENTRA MOUSE
+    function entraMouse(){
         avatarContainer.src = avatar2;
-    });
-
-avatarContainer.addEventListener("mouseout", function(){
+    }
+    // AÑADIR EVENTO DE SALIDA A LA ETIQUETA IMG
+    avatarContainer.addEventListener("mouseout", saleMouse);
+    // SALE MOUSE
+    function saleMouse(){
         avatarContainer.src =  avatar1;
-    });
+    }
+})();
 
 
