8b4331ee82df2e1e26388284f0cd11c40125f985
Juan Balbuena <ianivigo2@gmail.com>
2016-04-12 15:05:26 +0200

    Added comments

--- static/js/file.js
+++ static/js/file.js
@@ -5,18 +5,18 @@
 (function () {
     // GUARDAR ETIQUETA IMG PARA CAMBIAR SRC
     var avatarContainer = document.getElementById("avatar");
-    // GUARDAR URL ABSOLUTAS
+    // GUARDAR URLs ABSOLUTAS
     var avatar1 = "./static/img/juan-a.jpg";
     var avatar2 = "./static/img/juan-b.jpg";
-    // AÑADIR EVENTO DE ENTRADA A LA ETIQUETA IMG
+    // AÑADIR EVENTO DE ENTRADA DE MOUSE A LA ETIQUETA IMG
     avatarContainer.addEventListener("mouseover", entraMouse);
-    // ENTRA MOUSE
+    // ENTRA MOUSE Y CAMBIA FOTO
     function entraMouse(){
         avatarContainer.src = avatar2;
     }
-    // AÑADIR EVENTO DE SALIDA A LA ETIQUETA IMG
+    // AÑADIR EVENTO DE SALIDA DE MOUSE A LA ETIQUETA IMG
     avatarContainer.addEventListener("mouseout", saleMouse);
-    // SALE MOUSE
+    // SALE MOUSE Y CAMBIA FOTO
     function saleMouse(){
         avatarContainer.src =  avatar1;
     }
